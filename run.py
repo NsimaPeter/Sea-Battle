b47ef30f0b6f800ccb5712721b0ed9529fdc1b54
NsimaPeter <>
2023-05-30 11:16:24 +0000

    add the user guess,print and if/else statements and also add comments

--- run.py
+++ run.py
@@ -28,3 +28,27 @@ ship_col = random_col(board)
 # The Game loop
 for turn in range(4):
     print("Turn", turn + 1)
+
+    # Get user's guess
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
+
+    # Check if the guess is correct
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You sunk the battleship!")
+        break
+    else:
+        if guess_row not in range(5) or guess_col not in range(5):
+            print("Oops, that's not even in the ocean.")
+        elif board[guess_row][guess_col] == "X":
+            print("You guessed that one already.")
+        else:
+            print("You missed the battleship!")
+            board[guess_row][guess_col] = "X"
+        
+        # Print the updated game board
+        print_board(board)
+
+        # Check if the player has used all turns
+        if turn == 3:
+            print("Game Over! The battleship was located at (" + str(ship_row) + "," + str(ship_col) + ").")
